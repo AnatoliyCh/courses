a7c1da400a78d14ae897c6308189edf9f4ac0852
Anatoly <googlsocial97@gmail.com>
2024-01-12 02:36:33 +0700

    Тесты для FactorialZeros

---
+++ Алгоритмы и структуры данных - proglib.academy/Base.Tests/FactorialZerosTest.cs
@@ -0,0 +1,34 @@
+﻿namespace Base.Tests;
+
+public class FactorialZerosTest
+{
+    [Theory]
+    [InlineData(25, 6)]
+    [InlineData(1, 0)]
+    [InlineData(5, 1)]
+    public void GetFactorialZeros(int number, int expected)
+    {
+        // arrange    
+
+        // act
+        int zeros = FactorialZeros.GetFactorialZeros(number);
+
+        // assert
+        Assert.Equal(expected, zeros);
+    }
+
+    [Theory]
+    [InlineData(25, 6)]
+    [InlineData(1, 0)]
+    [InlineData(5, 1)]
+    public void GetFactorialZerosRecursion(int number, int expected)
+    {
+        // arrange
+
+        // act
+        int zeros = FactorialZeros.GetFactorialZerosRecursion(number);
+
+        // assert
+        Assert.Equal(expected, zeros);
+    }
+}
