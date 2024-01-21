beab069f2ebbf43c015fe55993f2a3ac0e7f6699
Anatoly <googlsocial97@gmail.com>
2024-01-21 08:45:01 +0700

    Разложение натурального числа n на простые множители

---
+++ Алгоритмы и структуры данных - proglib.academy/Base.Tests/PrimeFactorsTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Base.Tests;
+
+public class PrimeFactorsTest
+{
+    [Theory]
+    [InlineData(75, new int[] { 3, 5, 5 })]
+    [InlineData(20, new int[] { 2, 2, 5 })]
+    public void GetPrimeFactors(int number, int[] expected)
+    {
+        // arrange    
+
+        // act
+        var factors = PrimeFactors.GetPrimeFactors(number);
+
+        // assert
+        Assert.Equal(expected, factors);
+    }
+}
---
+++ Алгоритмы и структуры данных - proglib.academy/Base/1 Производительность алгоритмов/PrimeFactors.cs
@@ -0,0 +1,29 @@
+﻿namespace Base;
+
+/// <summary>
+///     Разложение натурального числа n на простые множители.
+///     75 -> 3 5 5
+///     20 -> 2 2 5
+/// </summary>
+public static class PrimeFactors
+{
+    public static int[] GetPrimeFactors(int number)
+    {
+        IList<int> factors = new List<int>();
+        for (int i = 2; i <= MathF.Sqrt(number); i++)
+        {
+            while (number % i == 0)
+            {
+                factors.Add(i);
+                number /= i;
+            }
+        }
+
+        if (number != 1)
+        {
+            factors.Add(number);
+        }
+
+        return factors.ToArray();
+    }
+}
