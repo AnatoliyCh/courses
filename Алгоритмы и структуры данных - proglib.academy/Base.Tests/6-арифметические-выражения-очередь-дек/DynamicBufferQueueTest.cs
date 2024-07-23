cbf806bf00f5d72317f18abd109bbad002a2b9df
Anatoly <googlsocial97@gmail.com>
2024-07-24 06:27:47 +0700

    1.6 Арифметические выражения. Очередь, дек
    Вычисление выражения
    Очередь с динамическим буфером

---
+++ Алгоритмы и структуры данных - proglib.academy/Base.Tests/6-арифметические-выражения-очередь-дек/DynamicBufferQueueTest.cs
@@ -0,0 +1,92 @@
+﻿using Base.ArithmeticExpressionsQueueDec;
+
+namespace Base.Tests.ArithmeticExpressionsQueueDec;
+
+public static class DynamicBufferQueueTest
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(3)]
+    [InlineData(5)]
+    public static void QueueInitBySizeTest(int size)
+    {
+        // arrange
+
+        // act
+        var result = new DynamicBufferQueue.Queue<int>(size);
+
+        // assert
+        Assert.NotNull(result);
+    }
+
+    [Theory]
+    [InlineData(0, new int[0]),]
+    [InlineData(3, new[] { 1, 2, 3 })]
+    [InlineData(5, new[] { 1, 2, 3, 4, 5 })]
+    public static void QueueIsEmptyTest(int size, int[] items)
+    {
+        // arrange
+        var queue = new DynamicBufferQueue.Queue<int>(size);
+
+        // act
+        foreach (var item in items)
+        {
+            queue.Enqueue(item);
+        }
+
+        while (!queue.IsEmpty)
+        {
+            queue.Dequeue();
+        }
+
+        // assert
+        Assert.True(queue.IsEmpty);
+    }
+
+    [Fact]
+    public static void QueueEnqueueDequeueTest()
+    {
+        // arrange
+        var queue = new DynamicBufferQueue.Queue<int>(2);
+
+        // act
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Dequeue();
+
+        // assert
+        Assert.False(queue.IsEmpty);
+    }
+
+    [Fact]
+    public static void QueueCircularTest()
+    {
+        // arrange
+        var queue = new DynamicBufferQueue.Queue<int>(2);
+
+        // act
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Dequeue();
+        queue.Enqueue(3);
+
+        // assert
+        Assert.False(queue.IsEmpty);
+    }
+
+    [Theory]
+    [InlineData("1", new[] { "3 44", "3 50", "2 44" }, "YES")]
+    [InlineData("2", new[] { "2 -1", "3 10" }, "YES")]
+    [InlineData("3", new[] { "3 44", "2 66" }, "NO")]
+    public static void ExecuteTest(string count, string[] commands, string expected)
+    {
+        // arrange
+
+        // act
+        var result = DynamicBufferQueue.Execute(Convert.ToInt32(count), commands);
+
+        // assert
+        Assert.Equal(expected, result);
+    }
+
+}
---
+++ Алгоритмы и структуры данных - proglib.academy/Base/6-арифметические-выражения-очередь-дек/DynamicBufferQueue.cs
@@ -0,0 +1,102 @@
+﻿namespace Base.ArithmeticExpressionsQueueDec;
+
+public static class DynamicBufferQueue
+{
+    public static readonly string resultYes = "YES";
+    public static readonly string resultNo = "NO";
+    public static readonly int commandPopFront = 2;
+    public static readonly int commandPushBack = 3;
+
+    public static string Execute(int count, string[] commands)
+    {
+        var queue = new Queue<int>(count);
+
+        foreach (var str in commands)
+        {
+            string[] splitStr = str.Trim().Split(' ');
+            var command = Convert.ToInt32(splitStr[0]);
+            var value = Convert.ToInt32(splitStr[1]);
+            if (commandPushBack == command)
+            {
+                queue.Enqueue(value);
+            }
+            else if (commandPopFront == command && !queue.IsEmpty)
+            {
+                var deqValue = queue.Dequeue();
+                if (deqValue != value)
+                {
+                    return resultNo;
+                }
+            }
+            else
+            {
+                // специальная команда для пустой структуры данных
+                if (value == -1 && commandPopFront == command)
+                {
+                    continue;
+                }
+
+                return resultNo;
+            }
+        }
+
+        return resultYes;
+    }
+
+    public class Queue<T>
+    {
+        private T[] buffer;
+        private int head; // первый элемент очереди
+        private int tail; // последний элемент очереди
+
+        public Queue(int size)
+        {
+            buffer = new T[size + 1];
+            head = tail = 0;
+        }
+
+        public bool IsEmpty { get => head == tail; }
+
+        public void Enqueue(T item)
+        {
+            // увеличение буфера и копирование
+            if ((tail + 1) % buffer.Length == head)
+            {
+                Resize();
+            }
+
+            buffer[tail] = item;
+            tail = (tail + 1) % buffer.Length;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            var result = buffer[head];
+            head = (head + 1) % buffer.Length;
+
+            return result;
+        }
+
+        private void Resize()
+        {
+            var newBuffer = new T[buffer.Length * 2];
+            int newBufferIndex = 0;
+            int bufferIndex = head;
+            while (bufferIndex != tail)
+            {
+                newBuffer[newBufferIndex] = buffer[bufferIndex];
+                newBufferIndex++;
+                bufferIndex = (bufferIndex + 1) % buffer.Length;
+            }
+
+            buffer = newBuffer;
+            head = 0;
+            tail = newBufferIndex;
+        }
+    }
+}
